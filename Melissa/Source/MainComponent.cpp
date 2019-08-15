6678bc4055b4160ca6764772425736702e3b4d11
mosynthkey <mo.synthkey.0112@gmail.com>
2019-08-15 13:14:37 +0900

    MainComponent : public Component -> AudioAppComponent

--- Melissa/Source/MainComponent.cpp
+++ Melissa/Source/MainComponent.cpp
@@ -1,4 +1,3 @@
-#include "../JuceLibraryCode/JuceHeader.h"
 #include "MainComponent.h"
 
 using std::make_unique;
@@ -16,7 +15,7 @@ private:
     {
         const auto& b = getLocalBounds();
         constexpr int t = 2; // thickness
-        g.setColour(juce::Colour::fromFloatRGBA(1.f, 1.f, 1.f, 0.3f));
+        g.setColour(juce::Colour::fromFloatRGBA(1.f, 1.f, 1.f, 0.4f));
         g.drawRoundedRectangle(t / 2, t / 2, b.getWidth() - t - 1, b.getHeight() - t - 1, (b.getHeight() - t) / 2, t);
     }
 };
@@ -40,12 +39,12 @@ private:
         const int triW = w * 9.f / 23.f;
         const int triH = h * 3.f / 7.f;
         
-        g.setColour(Colour::fromFloatRGBA(1.f, 1.f, 1.f, 0.3f));
+        g.setColour(Colour::fromFloatRGBA(1.f, 1.f, 1.f, 0.4f));
         g.drawEllipse(t / 2, t / 2, w - t - 1, h - t - 1, t);
         
         const int x0 = (w - triW) * 4.f / 7.f;
         const int y0 = (h - triH) / 2;
-    
+        
         Path path;
         path.addTriangle (x0, y0, x0, y0 + triH, x0 + triW, h / 2);
         g.fillPath(path);
@@ -66,10 +65,56 @@ playButton_(make_unique<MelissaPlayButton>("playButton"))
     addAndMakeVisible(button_.get());
     
     addAndMakeVisible(playButton_.get());
+
+    // Some platforms require permissions to open input channels so request that here
+    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
+        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+    {
+        RuntimePermissions::request (RuntimePermissions::recordAudio,
+                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
 }
 
 MainComponent::~MainComponent()
 {
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
+}
+
+void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+{
+    // Your audio-processing code goes here!
+
+    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+
+    // Right now we are not producing any data, in which case we need to clear the buffer
+    // (to prevent the output of random noise)
+    bufferToFill.clearActiveBufferRegion();
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
 }
 
 void MainComponent::paint (Graphics& g)
