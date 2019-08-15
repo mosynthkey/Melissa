a7829b3556efd33fa4da174237337da088f2fb68
mosynthkey <mo.synthkey.0112@gmail.com>
2019-08-15 12:22:24 +0900

    add melissa core and basic ui

---
+++ Melissa/Source/AudioLib/Melissa.h
@@ -0,0 +1,69 @@
+//
+//  Melissa.hpp
+//  Melissa_JUCE - App
+//
+//  Created by Masaki on 2019/07/29.
+//
+
+#pragma once
+
+#include "SoundTouch.h"
+#include <mutex>
+#include <vector>
+
+class Melissa
+{
+public:
+    Melissa();
+    virtual ~Melissa() {};
+    
+    void setBuffer(const float* buffer[], size_t bufferLength, int32_t sampleRate);
+    void setOutputSampleRate(int32_t sampleRate);
+    
+    void setAPosMSec(float aPosMSec);
+    void setBPosMSec(float bPosMSec);
+    void setPlayingPosMSec(float playingPosMSec);
+    int32_t getTotalLengthMSec() const;
+    int32_t getPlayingPosMSec() const;
+    
+    void setSpeed(float speed);
+    void setPitch(int32_t semitone);
+    
+    void render(float* bufferToRender[], size_t bufferLength);
+    
+    void process();
+    bool needToProcess();
+    float getProgress() const;
+    
+    void reset();
+    
+    // for debug
+    std::string getStatusString() const;
+    
+private:
+    std::unique_ptr<soundtouch::SoundTouch> soundTouch_;
+    
+    bool isOriginalBufferPrepared_;
+    std::vector<float> originalBuffer_[2 /* Stereo */];
+    int32_t originalSampleRate_;
+    
+    std::vector<float> processedBuffer_[2 /* Stereo */];
+    int32_t outputSampleRate_;
+    
+    size_t aIndex_, bIndex_;
+    size_t playingIndex_, readIndex_, writeIndex_;
+    
+    float   speed_;
+    int32_t semitone_;
+    
+    float   progress_;
+    bool    isProcessDone_;
+    
+    std::mutex mutex_;
+    bool isProcessedBufferPrepared_;
+    void prepareProcessedBuffer_();
+    
+    static constexpr size_t processLength_ = 4096;
+    float bufferForSoundTouch_[2 * processLength_];
+    int32_t totalReceivedSampleSize_;
+};
--- Melissa/Source/MainComponent.cpp
+++ Melissa/Source/MainComponent.cpp
@@ -1,83 +1,84 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
+#include "../JuceLibraryCode/JuceHeader.h"
 #include "MainComponent.h"
 
-//==============================================================================
-MainComponent::MainComponent()
-{
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize (800, 600);
+using std::make_unique;
 
-    // Some platforms require permissions to open input channels so request that here
-    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
-        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+class MelissaRoundButton : public Button
+{
+public:
+    MelissaRoundButton(const String& name) : Button(name)
     {
-        RuntimePermissions::request (RuntimePermissions::recordAudio,
-                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
+        setOpaque(false);
     }
-    else
+    
+private:
+    void paintButton (Graphics &g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
     {
-        // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        const auto& b = getLocalBounds();
+        constexpr int t = 2; // thickness
+        g.setColour(juce::Colour::fromFloatRGBA(1.f, 1.f, 1.f, 0.3f));
+        g.drawRoundedRectangle(t / 2, t / 2, b.getWidth() - t - 1, b.getHeight() - t - 1, (b.getHeight() - t) / 2, t);
     }
+};
+
+class MelissaPlayButton : public Button
+{
+public:
+    MelissaPlayButton(const String& name) : Button(name)
+    {
+        setOpaque(false);
+    }
+    
+private:
+    void paintButton (Graphics &g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+    {
+        const auto& b = getLocalBounds();
+        
+        constexpr int t = 2; // thickness
+        const int w = b.getWidth();
+        const int h = b.getHeight();
+        const int triW = w * 9.f / 23.f;
+        const int triH = h * 3.f / 7.f;
+        
+        g.setColour(Colour::fromFloatRGBA(1.f, 1.f, 1.f, 0.3f));
+        g.drawEllipse(t / 2, t / 2, w - t - 1, h - t - 1, t);
+        
+        const int x0 = (w - triW) * 4.f / 7.f;
+        const int y0 = (h - triH) / 2;
+    
+        Path path;
+        path.addTriangle (x0, y0, x0, y0 + triH, x0 + triW, h / 2);
+        g.fillPath(path);
+    }
+};
+
+MainComponent::MainComponent() :
+controlComponent_(make_unique<MelissaControlComponent>()),
+button_(make_unique<MelissaRoundButton>("testButton")),
+playButton_(make_unique<MelissaPlayButton>("playButton"))
+{
+    setSize (1400, 860);
+    
+    controlComponent_->setBounds(0, 240, getWidth(), 240);
+    addAndMakeVisible(controlComponent_.get());
+    
+    button_->setBounds(0, 240, 140, 40);
+    addAndMakeVisible(button_.get());
+    
+    addAndMakeVisible(playButton_.get());
 }
 
 MainComponent::~MainComponent()
 {
-    // This shuts down the audio device and clears the audio source.
-    shutdownAudio();
 }
 
-//==============================================================================
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
-{
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
-}
-
-void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
-{
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
-}
-
-void MainComponent::releaseResources()
-{
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
-}
-
-//==============================================================================
 void MainComponent::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    // You can add your drawing code here!
+    g.setGradientFill(ColourGradient(Colour(0xff019cdf), 0.f, 0.f, Colour(0xffc82788), getWidth(), getHeight(), true));
+    g.fillAll();
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    playButton_->setBounds((getWidth() - 100) / 2, 300, 100, 100);
 }
