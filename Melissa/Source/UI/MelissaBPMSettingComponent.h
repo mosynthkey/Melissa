b89003de640b275abff12a0aed0c64bcb5e649f8
Masaki Ono <mo.synthkey.0112@gmail.com>
2020-07-02 22:21:56 +0900

    bpm editor

--- Melissa/Source/UI/MelissaBPMSettingComponent.h
+++ Melissa/Source/UI/MelissaBPMSettingComponent.h
@@ -9,13 +9,17 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "MelissaLookAndFeel.h"
+#include "MelissaModelListener.h"
 #include "MelissaUtility.h"
 
-class MelissaBPMSettingComponent : public Component
+class MelissaBPMSettingComponent : public Component, public MelissaModelListener
 {
 public:
     MelissaBPMSettingComponent()
-    {        
+    {
+        auto model = MelissaModel::getInstance();
+        
+        
         constexpr int textEditorWidth = 100;
         constexpr int buttonWidth = 120;
         constexpr int controlHeight = 30;
@@ -58,6 +62,12 @@ public:
         bpmEditor_->setJustification(Justification::centred);
         bpmEditor_->setBounds(getWidth() - margin - textEditorWidth, margin, textEditorWidth, controlHeight);
         bpmEditor_->setInputRestrictions(3, "0123456789");
+        bpmEditor_->setText(String(model->getBpm()), dontSendNotification);
+        bpmEditor_->onReturnKey = [&, model]()
+        {
+            const auto bpm = bpmEditor_->getText().getIntValue();
+            model->setBpm(bpm);
+        };
         
         speedCheckBox_ = std::make_unique<ToggleButton>();
         speedCheckBox_->setButtonText("Correct with the playback speed.");
@@ -77,6 +87,11 @@ public:
     ~MelissaBPMSettingComponent()
     {
         speedCheckBox_->setLookAndFeel(nullptr);
+    }
+    
+    void bpmChanged(float bpm) override
+    {
+        bpmEditor_->setText(String(bpm), dontSendNotification);
     }
     
 private:
