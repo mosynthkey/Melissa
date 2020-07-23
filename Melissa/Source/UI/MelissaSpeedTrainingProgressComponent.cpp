94fb61aa57589ac07d8d4ce5c98f69c9002d5cdc
Masaki Ono <mo.synthkey.0112@gmail.com>
2020-07-23 15:49:09 +0900

    refactor

--- Melissa/Source/UI/MelissaSpeedTrainingProgressComponent.cpp
+++ Melissa/Source/UI/MelissaSpeedTrainingProgressComponent.cpp
@@ -38,7 +38,7 @@ void MelissaSpeedTrainingProgressComponent::paint(Graphics& g)
     g.setColour(Colours::white.withAlpha(0.2f));
     g.fillRoundedRectangle(triangleWidth / 2, 0, width - triangleWidth, lineHeight, lineHeight / 2);
     
-    const auto currentRatio = std::clamp((model_->getCurrentSpeed() - model_->getSpeedIncStart()) / static_cast<float>(model_->getSpeedIncGoal() - model_->getSpeedIncStart()), 0.f, 1.f);
+    const auto currentRatio = std::clamp((model_->getPlayingSpeed() - model_->getSpeedIncStart()) / static_cast<float>(model_->getSpeedIncGoal() - model_->getSpeedIncStart()), 0.f, 1.f);
     
     Path path;
     const int triangleCenter = currentRatio * (width - triangleWidth) + triangleWidth / 2;
@@ -49,7 +49,7 @@ void MelissaSpeedTrainingProgressComponent::paint(Graphics& g)
     g.setColour(Colour(MelissaUISettings::getAccentColour()));
     g.fillPath(path);
     
-    const auto currentSpeedStr = String(model_->getCurrentSpeed()) + "%";
+    const auto currentSpeedStr = String(model_->getPlayingSpeed()) + "%";
     const auto strSize = MelissaUtility::getStringSize(font_, currentSpeedStr);
     
     auto strX = std::clamp(triangleCenter - strSize.first / 2, 0, width - strSize.first);
